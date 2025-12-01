57cac186b082ac737ea3d179c96d4eea6322e631
Babacar Thiam <159429430+BabacarDev@users.noreply.github.com>
2025-12-01 12:53:08 +0000

    commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const sumbit = document.getElementById('submit');
+const errorMessage = document.getElementById('error-message');
+const email = document.getElementById('email');
+const sucessMessage = document.getElementById('sucess-message');
+
